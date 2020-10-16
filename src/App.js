3dbed75cbf73eb26098aa435227d32a46c081b12
LoicBrassart <loic.brassart@gmail.com>
2020-10-16 19:07:47 +0200

    Created a first character card with CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <section id="gallery">
+        <article className="character">
+          <img className="background" src="/img/backgrounds/outdoor-1.jpg" />
+          <img className="figure" src="/img/adventurers/amisandra/1-f.png" />
+          <footer>
+            <h3>A'misandra</h3>
+          </footer>
+        </article>
+      </section>
     </div>
   );
 }
