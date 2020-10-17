39774e2ad3604dfd34ab00fc67f9aeab891dd08e
LoicBrassart <loic.brassart@gmail.com>
2020-10-17 11:42:01 +0200

    Fixed console errors (*always* provide an 'alt' attribute to every <img> tag

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -8,8 +8,8 @@ export default function Character({name}) {
 
     return (
     <article className="character">
-        <img className="background" src="/img/backgrounds/outdoor-1.jpg" />
-        <img className="figure" src={`/img/adventurers/${cleanName}/1-f.png`} />
+        <img className="background" src="/img/backgrounds/outdoor-1.jpg" alt=""/>
+        <img className="figure" src={`/img/adventurers/${cleanName}/1-f.png`} alt={`name`}/>
         <footer>
             <h3>{name}</h3>
         </footer>
