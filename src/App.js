62487c4872dd9de4476118aeadadd09902d68c6e
LoicBrassart <loic.brassart@gmail.com>
2020-10-17 11:44:27 +0200

    Deleted code duplication + created a 'characters' array + used a map on characters

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Character from './components/Character';
 import "./reset.css";
 import "./style.css";
 
+const characters = ["A'misandra", "Lewto", "Nazuna", "Olber", "Zaldor"];
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <section id="gallery">
-        <Character name="A'misandra"/>
-        <Character name="Lewto"/>
-        <Character name="Nazuna"/>
-        <Character name="Olber"/>
-        <Character name="Zaldor"/>
+        {characters.map( (character)=>{
+          return <Character name={character}/>
+        } )}
       </section>
     </div>
   );
